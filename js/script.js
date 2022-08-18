67f8a4e787b0ca5d1e21c003b66a07adcb30c545
SSWFreelancer <97734977+SSWFreelancer@users.noreply.github.com>
2022-08-19 02:27:14 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $('.tabs__item').click(function (event) {
+          $(this).addClass('active');
+          $(".tabs__item").not(this).removeClass('active');
+    });    
+    $('.tabs__item-1').click(function(event){
+        $('#tab_01').addClass('target');
+        $('#tab_02').removeClass('target');
+        $('#tab_03').removeClass('target');
+        $('#tab_04').removeClass('target');      
+
+    });
+    $('.tabs__item-2').click(function(event){
+        $('#tab_02').addClass('target');
+        $('#tab_01').removeClass('target');
+        $('#tab_03').removeClass('target');
+        $('#tab_04').removeClass('target');
+    });
+    $('.tabs__item-3').click(function(event){
+        $('#tab_03').addClass('target');
+        $('#tab_01').removeClass('target');
+        $('#tab_02').removeClass('target');
+        $('#tab_04').removeClass('target');        
+    });
+    $('.tabs__item-4').click(function(event){
+        $('#tab_04').addClass('target');
+        $('#tab_01').removeClass('target');
+        $('#tab_03').removeClass('target');
+        $('#tab_02').removeClass('target');         
+    });	
+
+	 $('.content__block svg .svg').mouseenter(function(event){
+	  		$('.content__block svg').addClass('active')
+	 });
+	 $('.content__block svg').mouseleave(function(event){
+	  		$('.content__block svg').removeClass('active')
+	 });
+    $('.svg path').mouseenter(function(event){
+    	$(this).addClass('active');
+    });	
+    $('.svg path').mouseleave(function(event){
+    	$(this).removeClass('active');
+    });	 
+    $('.content__block svg .parking a').mouseenter(function(event){
+         $(this).addClass('active')
+    });
+    $('.content__block svg .parking a').mouseleave(function(event){
+         $(this).removeClass('active')
+    });
+   $('.cost__characteristic a').click(function (event) {
+      $('.request-popup').addClass('open');
+      $('body').addClass('lock');         
+   });        
+   $('.content__block.popup .svg path').click(function (event) {
+      $('.request-popup').addClass('open');
+      $('body').addClass('lock');         
+   });  
+   $('.content__block .parking').click(function (event) {
+      $('.request-popup').addClass('open');
+      $('body').addClass('lock');         
+   });  
+
+   $('.request-popup>p').click(function (event) {
+      $('.request-popup').removeClass('open');
+      $('body').removeClass('lock');         
+   });      
+   $('.request-popup__close').click(function (event) {
+      $('.request-popup').removeClass('open');
+      $('body').removeClass('lock');         
+   });    
+}); 	
